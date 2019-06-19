68e7482609d7573dd49349e411a65bfc94f842fa
Esteban Lara <esteban@esnilara.io>
2019-06-19 12:33:04 -0500

    chore: debugging-service-workers - fetch files from cache in sw

--- debugging-service-workers/service-worker.js
+++ debugging-service-workers/service-worker.js
@@ -30,6 +30,18 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request);
+    })
+  );
+});
+
 self.addEventListener('activate', function(event) {
   console.log('Finally active. Ready to start serving');
 });
