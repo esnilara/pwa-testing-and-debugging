acff1e4386b7b7dea0b0def218ac5c7a2c47437d
Esteban Lara <esteban@esnilara.io>
2019-06-19 12:18:32 -0500

    chore: debugging-service-workers - add cache files logic in sw.js

--- debugging-service-workers/service-worker.js
+++ debugging-service-workers/service-worker.js
@@ -1,21 +1,35 @@
 // Copyright 2016 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const CACHE_NAME = 'my-site-cache-v1';
+
+const URLS_TO_CACHE = [
+  '/',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/smiley.svg'
+];
+
 self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');  
+  console.log('Finally active. Ready to start serving');
 });
